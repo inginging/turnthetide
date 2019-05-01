5cba23e03ade381e306988d8d84fcbd73d52bdbc
Inge van Balgooi <inge@code-oranje.com>
2019-05-01 18:13:05 +0200

    Mobile

--- js/theme.js
+++ js/theme.js
@@ -6494,27 +6494,33 @@
 	}
 })(); 
 
-jQuery(document).ready(function($){
-    var current = location.pathname;
+jQuery(document).ready(function($) {
+	var current = location.pathname;
 
-    $('#navbarNavDropdown li a').each(function(){
-        var $this = $(this),
-            $thisUrl = $this[0].pathname;
+	$("#navbarNavDropdown li a").each(function() {
+		var $this = $(this),
+			$thisUrl = $this[0].pathname;
 
-            // if the current path is like this link, make it active
-        if ($thisUrl !== '/') {
-            if(current.indexOf($thisUrl) !== -1){
-                $this.parent().addClass('active');
-            }
-        }
-    })
-    
-    if($('body').hasClass('single-goals')) {
-        var goalId = $('#goalId').val(),
-            projectOption = $('select[name="dmm_project"]');
+		// if the current path is like this link, make it active
+		if ($thisUrl !== "/") {
+			if (current.indexOf($thisUrl) !== -1) {
+				$this.parent().addClass("active");
+			}
+		}
+	});
 
-        projectOption.val(goalId).attr('selected','selected'); 
+	if ($("body").hasClass("single-goals")) {
+		var goalId = $("#goalId").val(),
+			projectOption = $('select[name="dmm_project"]');
 
-    }
- 
+		projectOption.val(goalId).attr("selected", "selected");
+	}
+
+	$(".c-navbar-toggler").on("click", function() {
+		if ($("body").hasClass("lock-scroll")) {
+			$("body").removeClass("lock-scroll");
+		} else {
+			$("body").addClass("lock-scroll");
+		}
+	});
 });
