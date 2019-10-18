3827e3153e1855eb015d3b8615aba09039fddf85
Inge van Balgooi <inge@code-oranje.com>
2019-10-18 21:18:20 +0200

    removed console.log

--- inc/functions/function-mollie-donations-additions.php
+++ inc/functions/function-mollie-donations-additions.php
@@ -5,14 +5,11 @@
 
     function total_donations_reached_per_project($project, $post_id) {
             global $wpdb;
-            //$donations = $wpdb->get_var("SELECT SUM(dm_settlement_amount) FROM wp_donate_mollie WHERE dm_status='paid' AND dm_project='$project'" );
-            
-            //$donations = $wpdb->get_var("SELECT SUM(dm_settlement_amount) FROM wp_donate_mollie AND dm_project='$project'" );
-            $donations2 = $wpdb->get_var("SELECT SUM(dm_settlement_amount) FROM wp_donate_mollie WHERE dm_project='$project' AND dm_status='paid'");
-            
+
+            $donations2 = $wpdb->get_var("SELECT SUM(dm_settlement_amount) FROM wp_donate_mollie WHERE dm_project='$project' AND dm_status='paid'");   
             update_field('field_5bf6e2cfc337e', $donations2, $post_id); 
 
-                echo '<script>console.log('.json_encode($donations2).');</script>';
+            // echo '<script>console.log('.json_encode($donations2).');</script>';
     }
 
 ?>
